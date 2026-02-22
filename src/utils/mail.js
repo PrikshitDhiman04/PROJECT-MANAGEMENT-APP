8c3ea2a0ce5d1bec2caefacc9bd0ad1ab0fb38db
Prikshit Dhiman <dhimanprikshit04@gmail.com>
2026-02-22 13:10:05 +0530

    Added mail sending functionality using nodemailer

--- src/utils/mail.js
+++ src/utils/mail.js
@@ -1,4 +1,42 @@
 import Mailgen from "mailgen";
+import nodemailer from "nodemailer";
+
+const sendEmail = async (options) => {
+  const mailGenerator = new Mailgen({
+    theme: "default",
+    product: {
+      name: "Task Manager",
+      link: "https://taskmanagelink.com",
+    },
+  });
+  const emailTextual = mailGenerator.generatePlaintext(options.mailgenContent);
+  const emailHtml = mailGenerator.generate(options.mailgenContent);
+
+  const transporter = nodemailer.createTransport({
+    host: process.env.MAILTRAP_SMTP_HOST,
+    port: process.env.MAILTRAP_SMTP_PORT,
+    auth: {
+      user: process.env.MAILTRAP_SMTP_USER,
+      pass: process.env.MAILTRAP_SMTP_PASS,
+    },
+  });
+
+  const mail = {
+    from: "mail.taskmanager@example.com",
+    to: options.email,
+    subject: options.subject,
+    text: emailTextual,
+    html: emailHtml,
+  };
+
+  try {
+    await transporter.sendMail(mail);
+  } catch (error) {
+    console.error(
+      "Email service failed silently, make sure you have provided your mailtrap credentials in .env file",
+    );
+  }
+};
 
 const emailVerificationMailgenContent = (username, verificationUrl) => {
   return {
@@ -46,4 +84,5 @@ const forgotPasswordVerificationMailgenContent = (
 export {
   emailVerificationMailgenContent,
   forgotPasswordVerificationMailgenContent,
+  sendEmail,
 };
