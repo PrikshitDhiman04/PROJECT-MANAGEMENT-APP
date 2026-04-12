bf41b06b5659fe86e812462c7f30e4eb38e0e0e8
Prikshit Dhiman <dhimanprikshit04@gmail.com>
2026-04-12 11:58:13 +0530

    Project Creation Logic Added

--- src/controllers/project.controllers.js
+++ src/controllers/project.controllers.js
@@ -2,9 +2,29 @@ import { User } from "../models/users.models.js";
 import { Project } from "../models/project.models.js";
 import { ProjectMember } from "../models/projectmember.models.js";
 import { ApiError } from "../utils/api_errors.js";
+import { ApiRespose } from "../utils/api_response.js"
 import { asyncHandler } from "../utils/async-handler.js";
+import mongoose from "mongoose";
+import { UserRoleEnum } from "../utils/constants.js";
 
 const getProjects = asyncHandler(async (req, res) => {
+   const {name, description} = req.body
+
+   const project =await Project.create({
+     name,
+     description,
+     createdBy: new mongoose.Types.ObjectId(req.user._id),
+   });
+
+   await ProjectMember.create({
+     user: new mongoose.Types.ObjectId(req.user._id),
+     project: new mongoose.Types.ObjectId(project._id),
+     role: UserRoleEnum.ADMIN
+   });
+
+   return res
+    .status(201)
+    .json(new ApiRespose(201, project, "Project created Successfully"))
     
 })
 const getProjectById = asyncHandler(async (req, res) => {
