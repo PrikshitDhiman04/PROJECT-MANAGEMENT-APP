fe5f9798c7238f6b2e907924d61465d4cb27a067
Prikshit Dhiman <dhimanprikshit04@gmail.com>
2026-04-13 11:41:07 +0530

    Small Bug Fix

--- src/controllers/project.controllers.js
+++ src/controllers/project.controllers.js
@@ -7,7 +7,9 @@ import { asyncHandler } from "../utils/async-handler.js";
 import mongoose from "mongoose";
 import { UserRoleEnum } from "../utils/constants.js";
 
-const getProjects = asyncHandler(async (req, res) => {});
+const getProjects = asyncHandler(async (req, res) => {
+  const projects = await ProjectMember
+});
 const getProjectById = asyncHandler(async (req, res) => {});
 const createProject = asyncHandler(async (req, res) => {
   const { name, description } = req.body;
--- src/models/projectmember.models.js
+++ src/models/projectmember.models.js
@@ -23,4 +23,4 @@ const projectMemberSchema = new Schema(
   { timestamps: true },
 );
 
-export const ProjectMember = mongoose.model("Project", projectMemberSchema);
+export const ProjectMember = mongoose.model("ProjectMember", projectMemberSchema);
