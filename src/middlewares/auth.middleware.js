17b0c3c78f077eab7a2d7bc7c07ac2a152becc2b
Prikshit Dhiman <dhimanprikshit04@gmail.com>
2026-04-27 18:07:07 +0530

    Role Base Permissions

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -1,3 +1,5 @@
+import mongoose from "mongoose";
+import { ProjectMember } from "../models/projectmember.models.js";
 import { User } from "../models/users.models.js";
 import { ApiError } from "../utils/api_errors.js";
 import { asyncHandler } from "../utils/async-handler.js";
@@ -27,3 +29,33 @@ export const verifyJWT = asyncHandler(async (req, res, next) => {
     throw new ApiError(401, "Invalid access Token");
   }
 });
+
+export const validateProjectPermission = (roles = []) => {
+  asyncHandler(async (req, res, next) => {
+    const { projectId } = req.params;
+
+    if (!projectId) {
+      throw new ApiError(400, "project id is missing");
+    }
+
+    const project = await ProjectMember.findOne({
+      project: new mongoose.Types.ObjectId(projectId),
+      user: new mongoose.Types.ObjectId(req.user_id),
+    });
+
+    if (!project) {
+      throw new ApiError(400, "project not found");
+    }
+
+    const givenRole = project?.role;
+
+    req.user.role = givenRole;
+
+    if (!roles.includes(givenRole)) {
+      throw new ApiError(
+        403,
+        "You do not have permission to perform this action",
+      );
+    }
+  });
+};
